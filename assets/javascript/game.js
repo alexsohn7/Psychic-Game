5fe9216e7737d23420dd8ed212f0a1b4264b96cb
alexsohn7@gmail.com <alexsohn7@gmail.com>
2018-06-14 09:39:31 -0700

    initial site files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,50 @@
 // Variables
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+var wins = 0;
+var losses = 0;
+var numberOfGuesses = 9;
+var guessChoices = [];
 
-		 var wins = 0;
-		 var losses = 0;
-		 var numGuesses = 9;
-		 var guessChoices = [];
+document.onkeyup = function(event) {
 
-		 document.onkeyup = function(event) {
+// Declared variables within local scope.  Assigned when key is presseed by user
+    var userGuess = event.key;
 
-		 	var userGuess = event.key;
+	var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-		 	var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-		 	var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+	var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 		 	
-
-			if (options.indexOf(userGuess) > -1) {
-
+// conditional statement if user chooses a letter
+	if (letters.indexOf(userGuess) > -1) {
+// if user guesses letter correctly 
 				if (userGuess === computerGuess) {
 					wins++;
-					numGuesses = 9;
+					numberofGuesses = 9;
 					guessChoices = [];
 				}
-
+// if user does not guess letter correctly 
 				if (userGuess != computerGuess) {
-					numGuesses --;
+					numberofGuesses --;
 					guessChoices.push(userGuess);
 				}
+// when user runs out of guesses
+				if (numberOfGuesses === 0) {
 
-				if (numGuesses === 0) {
-
-				numGuesses = 9;
+				numberOfGuesses = 9;
 				losses ++;
 				guessChoices = [];
 
 				
 			}
-
+// variable html that replaces game id in html file. 
 			var html = 
 			"<h1> The Psychic Game </h1>" +
 			"<p>Guess what letter I'm thinking of!</p>" +
 			"<p>Wins: " + wins + "</p>" +
 			"<p>Losses: " + losses + "</p>" +
-			"<p>Guesses Left: " + numGuesses + "</p>" +
+			"<p>Guesses Left: " + numberOfGuesses + "</p>" +
 			"<p>Your Guesses so far: " + guessChoices.join(", ") + "</p>";
-
+// DOM replaces entire game id in html file with variable html
 			document.querySelector("#game").innerHTML = html;
 
 			
